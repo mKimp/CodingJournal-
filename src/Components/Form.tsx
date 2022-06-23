6f3177a8fc0133e97600879dfd7283156dc0cb1c
Kim Ma <kim.ma@brandlive.com>
2022-06-23 11:12:09 -0700

    fixed typescript errors on inputs

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,27 +1,46 @@
 import { useState } from "react"
+import { text } from "stream/consumers";
 
 export const Form = () => {
-  const [inputs, setInputs] = useState();
+  type Target = {
+    name:String;
+    value:String
+  }
+  const initialiValues = {
+    questionName: "",
+    codingNumber: null,
+    codingSource: null,
+  }
+  const [inputs, setInputs] = useState(initialiValues);
+  const [textArea, setTextArea] = useState("");
 
-  const handleOnchange = () => {
-    
+  const handleOnchange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const event = e.target as HTMLInputElement;
+    const value = event.value;
+    const name = event.name;
+    setInputs({...inputs, [name]:value})
+    console.log(inputs);
+  } 
+
+  const handleTextArea = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTextArea(e.target.value);
+    console.log(textArea);
   }
 
+  const handleSubmit = () => {
 
-
-
-
+  }
   return(
     <div className="form-wrapper">
       <form className="question-form">
           <label htmlFor="question">Question Name</label>
-          <input type="text" id="question"/>
+          <input name="question" type="text" id="question" onChange={(e) => handleOnchange(e)}/>
 
           <label htmlFor="codingNumber">Coding Number (optional)</label>
-          <input type="text" id="codingNumber"/>
+          <input name="codingNumber" type="text" id="codingNumber"  onChange={(e) => handleOnchange(e)}/>
 
           <label htmlFor="codingSource">Coding Source(optional)</label>
-          <input type="text" id="codingSource"/>
+          <input name="codingSource" type="text" id="codingSource" onChange={(e) => handleOnchange(e)}/>
 
         <label htmlFor="categories">Categories</label>
         <select name="categories" id="catergories">
@@ -30,8 +49,8 @@ export const Form = () => {
           <option value="twoPointers">Two Pointers</option>
 
         </select>
-
-        <textarea name="note" id="note">This is a test area</textarea>
+        <textarea value={textArea} name="note" id="note" placeholder="Your note here"onChange={(e) => handleTextArea(e)}>This is a test area</textarea>
+        <button className="btn-submit" onClick={handleSubmit}>Submit</button>
       </form>
     </div>
   )
