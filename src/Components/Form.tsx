c29042bb487d931a4c2f1da84cb42e9d697d1f53
Kim Ma <kim.ma@brandlive.com>
2022-06-23 11:38:55 -0700

    finished the form flow

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,18 +1,16 @@
 import { useState } from "react"
-import { text } from "stream/consumers";
 
 export const Form = () => {
-  type Target = {
-    name:String;
-    value:String
-  }
+
   const initialiValues = {
     questionName: "",
     codingNumber: null,
     codingSource: null,
   }
+
   const [inputs, setInputs] = useState(initialiValues);
   const [textArea, setTextArea] = useState("");
+  const [selectedValue, setSelectedValue] = useState("dynamicProgramming");
 
   const handleOnchange = (e:React.ChangeEvent<HTMLInputElement>) => {
     const event = e.target as HTMLInputElement;
@@ -27,10 +25,16 @@ export const Form = () => {
     console.log(textArea);
   }
 
-  const handleSubmit = () => {
+  const handleSelected = (e: React.ChangeEvent<HTMLSelectElement>) => {
+   setSelectedValue(e.target.value);
+  }
 
+  const handleSubmit = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    console.log(selectedValue)
   }
   return(
+    
     <div className="form-wrapper">
       <form className="question-form">
           <label htmlFor="question">Question Name</label>
@@ -43,14 +47,14 @@ export const Form = () => {
           <input name="codingSource" type="text" id="codingSource" onChange={(e) => handleOnchange(e)}/>
 
         <label htmlFor="categories">Categories</label>
-        <select name="categories" id="catergories">
-          <option value="DynamicProgramming">Dynamic Programming</option>
-          <option value="Backtracking">Backtracking</option>
+        <select name="categories" id="catergories" value={selectedValue} onChange={(e) => handleSelected(e)}>
+          <option value="dynamicProgramming">Dynamic Programming</option>
+          <option value="backTracking">Backtracking</option>
           <option value="twoPointers">Two Pointers</option>
 
         </select>
         <textarea value={textArea} name="note" id="note" placeholder="Your note here"onChange={(e) => handleTextArea(e)}>This is a test area</textarea>
-        <button className="btn-submit" onClick={handleSubmit}>Submit</button>
+        <button className="btn-submit" onClick={(e) => handleSubmit(e)}>Submit</button>
       </form>
     </div>
   )
